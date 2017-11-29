e95aa9ee3f11eb2c2d61af55924980694cd38ead
Jacob Avlund <jacob@avlund.dk>
2017-11-30 00:21:55 +0100

    Additional files

---
+++ examples/promises/mongowithasync.js
@@ -0,0 +1,23 @@
+const mongo = require('mongodb');
+
+const client = mongo.MongoClient;
+let database = null;
+let collection = null;
+
+client
+  .connect('mongodb://localhost/mydb')
+  .then((db) => {
+    database = db;
+    collection = database.collection('people');
+    return collection.insert({name : 'Ann'});
+  })
+  .then(docs => {
+    return collection.find().toArray();
+  })
+  .then(results => {
+    console.log(results);
+    database.close();
+  })
+  .catch(err => {
+    console.error('Error occured:', err);
+  });
